4e13444add51afdd573c88c0680edd6221cb2a8f
Nandy Bâ <nandyba@yahoo.fr>
2022-10-30 09:08:47 +0000

    Change MomentoHub smart-contract

--- src/hooks/useMomentoHub.js
+++ src/hooks/useMomentoHub.js
@@ -4,7 +4,7 @@ import { useWeb3PolygonTestnet } from './useWeb3.js';
 
 const useMomentoHub = () => {
     const web3 = useWeb3PolygonTestnet();
-    const MomentoHubContractAddress = web3.utils.toChecksumAddress('0x92e16023C1201aEf432cEb15677791AE03966De6');
+    const MomentoHubContractAddress = web3.utils.toChecksumAddress('0x2C36fD1981465F40B199f8A869B2D2f8F9692F36');
     const MomentoHub = new web3.eth.Contract(MomentoHubABI, MomentoHubContractAddress);
     return MomentoHub;
 };
