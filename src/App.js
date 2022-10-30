3924fbe5fc183b375e044af24c46e3cefc988f88
Nandy Bâ <nandyba@yahoo.fr>
2022-10-30 01:54:25 +0100

    Change wallet connect buttom page emplacement

--- src/App.js
+++ src/App.js
@@ -6,6 +6,32 @@ import MapView from "./Pages/Map-view";
 import WalletConnectScreen from "./Pages/Wallet-connect";
 import CreateNFTView from "./Pages/Create-NFT-view";
 
+import { Web3Modal } from '@web3modal/react';
+import { providers } from '@web3modal/ethereum';
+import { gnosisTestnet, cronosTestnet, mumbaiTestnet } from "./utils/network";
+
+const config = {
+  projectId: '1bd4139fcba9da0ebb55e2d5ffa1d12d',
+  theme: 'dark',
+  accentColor: 'default',
+  ethereum: {
+    appName: 'Momento',
+    autoConnect: true,
+    chains: [
+      gnosisTestnet,
+      cronosTestnet,
+      mumbaiTestnet,
+    ],
+    providers: [
+      providers.walletConnectProvider({
+        projectId: "1bd4139fcba9da0ebb55e2d5ffa1d12d",
+      }),
+    ],
+  }
+};
+
+
+
 
 
 function App() {
@@ -31,13 +57,17 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <PostListView /> */}
-        {/* <OurMap/> */}
-      </header>
-      {componentToRender()}
-    </div>
+    <>
+      <Web3Modal config={config}/>
+      <div className="App">
+        <header className="App-header">
+          
+          {/* <PostListView /> */}
+          {/* <OurMap/> */}
+        </header>
+        {componentToRender()}
+      </div>
+    </>
   );
 }
 
