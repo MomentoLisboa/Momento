5e9490f140fb6394c6713e40c48344f0fec528e1
Nandy Bâ <nandyba@yahoo.fr>
2022-10-30 06:34:40 +0000

    Get map zoom

--- src/componants/Map/index.js
+++ src/componants/Map/index.js
@@ -3,7 +3,8 @@ import {
     MapContainer,
     TileLayer,
     Circle,
-    Popup
+    Popup,
+    useMapEvents
 } from 'react-leaflet';
 import { useEffect, useState } from 'react';
 
@@ -11,9 +12,15 @@ import 'leaflet/dist/leaflet.css';
 
 import './MapView.css'
 
+const Map = ({MomentoNFTs, initialZoom}) => {
 
-const Map = ({ MomentoNFTs }) => {
-
+    const [zoomLevel, setZoomLevel] = useState(initialZoom); // initial zoom level provided for MapContainer
+    
+    const mapEvents = useMapEvents({
+        zoomend: () => {
+            setZoomLevel(mapEvents.getZoom());
+        },
+    });
     const [info, setInfo] = useState(undefined);
     const countdownTime = 5000;
     const [countdown, setCountdown] = useState(()=>{});
@@ -23,9 +30,6 @@ const Map = ({ MomentoNFTs }) => {
     //     iconRetinaUrl: "/images/marker-icon.png",
     //     shadowUrl: "/images/marker-shadow.png"
     // });
-
-    const starterPosition = MomentoNFTs.length > 0 ? [MomentoNFTs?.properties?.coords[0].latitude, MomentoNFTs?.properties?.coords[0].longitude] : [38.71307519909844, -9.149991653229893];
-
 
     if(info?.name !== undefined){
         console.log(info?.name)
@@ -44,31 +48,44 @@ const Map = ({ MomentoNFTs }) => {
     return (
         <>
             <div style={{color: 'white'}}>{info?.name}</div>
-            <MapContainer className="map homeMap" center={starterPosition} zoom={12} scrollWheelZoom={false}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                {MomentoNFTs.map((URI, index) => {
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {MomentoNFTs.map((URI, index) => {
 
-                    const coord = {
-                        latitude: URI?.properties.coords.latitude,
-                        longitude: URI?.properties.coords.longitude,
-                        name: URI?.name,
-                        description: URI?.description,
-                        key: index
-                    }
+                const coord = {
+                    latitude: URI?.properties.coords.latitude,
+                    longitude: URI?.properties.coords.longitude,
+                    name: URI?.name,
+                    description: URI?.description,
+                    key: index
+                }
 
-                    return(
-                        <>
-                            <Circle className="circle-magic-kingdom" center={[coord.latitude, coord.longitude]} radius={200} eventHandlers={{ click: () => setInfo(URI) }}/>
-                            <Circle className="circle-magic-kingdom" center={[coord.latitude, coord.longitude]} radius={200} eventHandlers={{ click: () => setInfo(URI) }}/>
-                        </>
-                    )
-                })}
-            </MapContainer>
+                return(
+                    <>
+                        <Circle className="circle-magic-kingdom" center={[coord.latitude, coord.longitude]} radius={200} eventHandlers={{ click: () => setInfo(URI) }}/>
+                        <Circle className="circle-magic-kingdom" center={[coord.latitude, coord.longitude]} radius={200} eventHandlers={{ click: () => setInfo(URI) }}/>
+                    </>
+                )
+            })}
         </>
     )
 }
 
-export default Map;
+const MapCustomContainer = ({ MomentoNFTs }) => {
+    
+    const initialZoom = 12
+
+    const starterPosition = MomentoNFTs.length > 0 ? [MomentoNFTs?.properties?.coords[0].latitude, MomentoNFTs?.properties?.coords[0].longitude] : [38.71307519909844, -9.149991653229893];
+
+    return (
+        <MapContainer className="map homeMap" center={starterPosition} zoom={initialZoom} scrollWheelZoom={false}>
+            <Map MomentoNFTs={MomentoNFTs} initialZoom={initialZoom} />
+        </MapContainer>
+    )
+}
+
+
+export default MapCustomContainer;
+
