76c3f2a9a7ed149a90d8e3661c01fc87c8370c7a
Nandy Bâ <nandyba@yahoo.fr>
2022-10-30 02:44:37 +0000

    Add checkWhitelist for Lens

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import MapView from "./Pages/Map-view";
 import WalletConnectScreen from "./Pages/Wallet-connect";
 import CreateNFTView from "./Pages/Create-NFT-view";
 import Home from "./Pages/Home";
+import CheckWhitelist from "./Pages/CheckWhitelist";
 
 import { Web3Modal } from '@web3modal/react';
 import { providers } from '@web3modal/ethereum';
@@ -63,6 +64,8 @@ function App() {
         return <CreateNFTView appState={appState} goToMap={goToMap} goToList={goToList}/>;
       case "list":
         return <PostListView />
+      case "checkWhitelist":
+        return <CheckWhitelist />
       default:
         return <WalletConnectScreen changeConnectedWallet={setAppState}/>;
     }
--- src/hooks/useMomentoHub.js
+++ src/hooks/useMomentoHub.js
@@ -1,9 +1,9 @@
 import MomentoHubABI from '../ABI/MomentoHubABI';
-import { useWeb3 } from './useWeb3.js';
+import { useWeb3PolygonTestnet } from './useWeb3.js';
 
 
 const useMomentoHub = () => {
-    const web3 = useWeb3();
+    const web3 = useWeb3PolygonTestnet();
     const MomentoHubContractAddress = web3.utils.toChecksumAddress('0x92e16023C1201aEf432cEb15677791AE03966De6');
     const MomentoHub = new web3.eth.Contract(MomentoHubABI, MomentoHubContractAddress);
     return MomentoHub;
